266557fac70c4d4918f2d39a224211ff05ea8e40
Arnas Milaševičius <arnasmila@gmail.com>
2020-01-09 14:11:05 +0200

    add hostinger-task

---
+++ hostinger-task/precache-manifest.452dd555de3c111e22b91e2a30eb28b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b14e5477a5deb465db94031d450ff75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10d9aee22c7062def879",
+    "url": "/static/css/main.ca69b9c0.chunk.css"
+  },
+  {
+    "revision": "b951cb3f22256fbc6477",
+    "url": "/static/js/2.2a0c2ca8.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.2a0c2ca8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10d9aee22c7062def879",
+    "url": "/static/js/main.1f8b7b30.chunk.js"
+  },
+  {
+    "revision": "452e67941dc3c2ba354e",
+    "url": "/static/js/runtime-main.97a4cd26.js"
+  },
+  {
+    "revision": "412f32896c37205f333f5c7fa9b77585",
+    "url": "/static/media/arrow-down.412f3289.svg"
+  },
+  {
+    "revision": "a7254bd244e6b894df14e83d950daf83",
+    "url": "/static/media/arrow-up.a7254bd2.svg"
+  }
+]);
